9e038a59368c00688f791353939e81deb7ba9dd8
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-01-24 22:15:14 +0100

    fixing missing varibale definiton

--- resources.tf
+++ resources.tf
@@ -67,14 +67,14 @@ resource "azurerm_storage_account" "storage_account" {
 
 
 data "azurerm_function_app_host_keys" "this" {
-  count               = var.use_private_net ? 0 : 1
+  count               = var.enable_private_access ? 0 : 1
   depends_on          = [azurerm_linux_function_app.this]
   name                = azurerm_linux_function_app.this.name
-  resource_group_name = var.resource_group
+  resource_group_name = var.resource_group_name
 }
 
 data "azurerm_function_app_host_keys" "this_vnet" {
-  count               = var.use_private_net ? 1 : 0
+  count               = var.enable_private_access ? 1 : 0
   depends_on          = [azurerm_linux_function_app.this, azurerm_app_service_virtual_network_swift_connection.this[0]]
   name                = azurerm_linux_function_app.this.name
   resource_group_name = var.resource_group
