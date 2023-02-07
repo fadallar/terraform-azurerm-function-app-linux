31bd1f108eec948de4051945eb9806436a89bc90
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-02-07 07:59:06 +0100

    Fixing typos

--- locals.tf
+++ locals.tf
@@ -2,12 +2,12 @@ locals {
   app_settings = {
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = "true"
     WEBSITE_ENABLE_SYNC_UPDATE_SITE     = "true"
-    JAVA_OPTS                           = "-Dlog4j2.formatMsgNoLookups=true"
-    LOG4J_FORMAT_MSG_NO_LOOKUPS         = "true"
+    #JAVA_OPTS                           = "-Dlog4j2.formatMsgNoLookups=true"
+    #LOG4J_FORMAT_MSG_NO_LOOKUPS         = "true"
     WEBSITE_USE_PLACEHOLDER             = "0"
     AZURE_LOG_LEVEL                     = "info"
     AzureWebJobsDisableHomepage         = "true"
-    AzureWebJobsStorage__accountName    = local.storage_account_name
+    AzureWebJobsStorage__accountName    = local.storage_name
     ### To-Do review below naming structure 
     ##  AzureFunctionsWebHost__hostid       = substr("fn-${var.project}-${var.env}-${var.location}-${var.name}", -32, -1)
   }
