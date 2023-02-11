6fdee0d146a9700756fa17537befa1ba5de38cf8
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-02-11 19:06:17 +0100

    debugging

--- resources.tf
+++ resources.tf
@@ -1,5 +1,4 @@
 resource "azurerm_linux_function_app" "this" {
-  depends_on = 
   name                          = local.name
   location                      = var.location
   resource_group_name           = var.resource_group_name
