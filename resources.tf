1aaf9b55ba6f88e0af8225a2335b46e0b6826257
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-02-11 20:39:56 +0100

    debugging

--- resources.tf
+++ resources.tf
@@ -2,8 +2,8 @@ resource "azurerm_linux_function_app" "this" {
   name                          = local.name
   location                      = var.location
   resource_group_name           = var.resource_group_name
-  storage_account_name       = azurerm_storage_account.storage_account.name
-  storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
+  #storage_account_name       = azurerm_storage_account.storage_account.name
+  #storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
   service_plan_id               = var.service_plan_id
   storage_uses_managed_identity = var.storage_uses_managed_identity
   https_only                    = true
