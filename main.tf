2edc81a66a1bc044285cd031b3f9a83e447e8c31
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-03-18 09:07:29 +0100

    trimdown to troubleshoot

--- main.tf
+++ main.tf
@@ -7,38 +7,40 @@ resource "azurerm_linux_function_app" "this" {
   storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
   service_plan_id            = var.service_plan_id
   #storage_uses_managed_identity   = var.storage_uses_managed_identity
-  https_only                      = var.https_only
-  enabled                         = true
-  builtin_logging_enabled         = var.builtin_logging_enabled
-  functions_extension_version     = var.functions_extension_version
-  tags                            = merge(var.default_tags, var.extra_tags)
+  #https_only                      = var.https_only
+  #enabled                         = true
+  #builtin_logging_enabled         = var.builtin_logging_enabled
+  #functions_extension_version     = var.functions_extension_version
+  #tags                            = merge(var.default_tags, var.extra_tags)
   #app_settings                    = merge(local.app_settings, var.app_settings)
-  key_vault_reference_identity_id = var.key_vault_identity_id
-  identity {
-    type         = var.identity_ids == null ? "SystemAssigned" : "SystemAssigned, UserAssigned"
-    identity_ids = var.identity_ids
-  }
-  site_config {
-    ## To-DO Evaluate below site config vs best practices
-    ## 
-    #application_insights_connection_string = var.enable_appinsights ? var.application_insights_connection_string : null
-    always_on = true
-    vnet_route_all_enabled = var.vnet_route_all_enabled
-    #ftps_state                             = "Disabled"
-    #http2_enabled                          = true
-    #websockets_enabled                     = false
-    #use_32_bit_worker                      = false
-    #ip_restriction                         = var.ip_restriction
-    #scm_ip_restriction                     = var.ip_restriction
-    application_stack {
-      dotnet_version              = local.application_stack.dotnet_version
-      use_dotnet_isolated_runtime = local.application_stack.use_dotnet_isolated_runtime
-      java_version                = local.application_stack.java_version
-      node_version                = local.application_stack.node_version
-      python_version              = local.application_stack.python_version
-      powershell_core_version     = local.application_stack.powershell_core_version
-      use_custom_runtime          = local.application_stack.use_custom_runtime
-    }
+  #key_vault_reference_identity_id = var.key_vault_identity_id
+  #identity {
+  #  type         = var.identity_ids == null ? "SystemAssigned" : "SystemAssigned, UserAssigned"
+  #  identity_ids = var.identity_ids
+  #}
+
+  site_config = {}
+  #site_config {
+  #  ## To-DO Evaluate below site config vs best practices
+  #  ## 
+  #  #application_insights_connection_string = var.enable_appinsights ? var.application_insights_connection_string : null
+  #  always_on = true
+  #  vnet_route_all_enabled = var.vnet_route_all_enabled
+  #  #ftps_state                             = "Disabled"
+  #  #http2_enabled                          = true
+  #  #websockets_enabled                     = false
+  #  #use_32_bit_worker                      = false
+  #  #ip_restriction                         = var.ip_restriction
+  #  #scm_ip_restriction                     = var.ip_restriction
+  #  application_stack {
+  #    dotnet_version              = local.application_stack.dotnet_version
+  #    use_dotnet_isolated_runtime = local.application_stack.use_dotnet_isolated_runtime
+  #    java_version                = local.application_stack.java_version
+  #    node_version                = local.application_stack.node_version
+  #    python_version              = local.application_stack.python_version
+  #    powershell_core_version     = local.application_stack.powershell_core_version
+  #    use_custom_runtime          = local.application_stack.use_custom_runtime
+  #  }
   }
   #lifecycle {
   #  # To-DO evaluate the reasoning behing these ignore changes
@@ -59,11 +61,11 @@ resource "azurerm_linux_function_app" "this" {
 #
 ### To-DO Create KeyVault Secret with Azure File Connection String
 ### And INject it in app-settings
-resource "azurerm_storage_share" "this" {
-  name                 = "functionshare"
-  storage_account_name = azurerm_storage_account.storage_account.name
-  quota                = 50
-}
+#resource "azurerm_storage_share" "this" {
+#  name                 = "functionshare"
+#  storage_account_name = azurerm_storage_account.storage_account.name
+#  quota                = 50
+#}
 
 resource "azurerm_storage_account" "storage_account" {
   name                             = local.storage_name
